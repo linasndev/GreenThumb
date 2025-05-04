5ce6efb27b7754cfbb1c40544ebb877c2970c6c9
Linas <linas404@protonmail.com>
2025-05-04 19:36:23 +0200

    Initial Commit

---
+++ GreenThumb/GreenThumbApp.swift
@@ -0,0 +1,17 @@
+//
+//  GreenThumbApp.swift
+//  GreenThumb
+//
+//  Created by Linas on 04/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct GreenThumbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
